8ce85a4b683f05d5097096c2e363497badd08ec9
TheElegantCoding <luismonsalve1941@gmail.com>
2026-05-07 23:10:32 -0400

    feat: refactor language utility functions for improved readability and consistency

--- src/global/util/language.ts
+++ src/global/util/language.ts
@@ -3,47 +3,50 @@ import { removeTrailingSlash } from '@global/util/link';
 
 const { defaultLanguage, languages } = siteConfiguration;
 
-type LanguageParameters =
-{
+type LanguageParameters = {
   params: Record<string, string>;
 };
 
 type LanguageType = keyof typeof languages;
 
-const getLanguageFromUrl = (url: URL): LanguageType =>
-{
+const getLanguageFromUrl = (url: URL): LanguageType => {
   const urlLang = url.pathname.split('/')[1];
 
-  if(urlLang && urlLang in languages)
-  {
+  if (urlLang && urlLang in languages) {
     return urlLang as LanguageType;
   }
 
   return defaultLanguage as LanguageType;
 };
 
-const getStaticLanguage = () =>
-{
+const validateLanguage = (language: string): LanguageType => {
+  if (language in languages) {
+    return language as LanguageType;
+  }
+
+  return defaultLanguage as LanguageType;
+};
+
+const getStaticLanguage = () => {
   const language: LanguageParameters[] = [];
 
-  Object.keys(languages).forEach((key) =>
-  {
+  Object.keys(languages).forEach((key) => {
     language.push({ params: { language: key } });
   });
 
   return language;
 };
 
-const getLanguagePathname = (url: URL) =>
-{
+const getLanguagePathname = (url: URL) => {
   const modifiedUrl = url;
 
   const languagePrefixes = Object.keys(languages);
-  const hasLanguagePrefix = languagePrefixes.some((prefix) => modifiedUrl.pathname.includes(prefix));
+  const hasLanguagePrefix = languagePrefixes.some((prefix) => { return modifiedUrl.pathname.includes(prefix); });
 
-  if(hasLanguagePrefix)
-  {
-    modifiedUrl.pathname = modifiedUrl.pathname.replace(new RegExp(`/(${languagePrefixes.join('|')})`, 'u'), '');
+  if (hasLanguagePrefix) {
+    const regex = new RegExp(`^/(${languagePrefixes.join('|')})(?=/|$)`, 'u');
+
+    modifiedUrl.pathname = modifiedUrl.pathname.replace(regex, '');
 
     return modifiedUrl.pathname;
   }
@@ -51,16 +54,13 @@ const getLanguagePathname = (url: URL) =>
   return modifiedUrl.pathname;
 };
 
-const addLanguageToPathname = (url: URL, language: LanguageType) =>
-{
+const addLanguageToPathname = (url: URL, language: LanguageType) => {
   const modifiedUrl = url;
 
-  if(language !== defaultLanguage)
-  {
+  if (language !== defaultLanguage) {
     modifiedUrl.pathname = `/${language}${modifiedUrl.pathname}`;
 
-    if(modifiedUrl.hash)
-    {
+    if (modifiedUrl.hash) {
       modifiedUrl.href = modifiedUrl.href.replaceAll(`/${modifiedUrl.hash}`, modifiedUrl.hash);
     }
   }
@@ -68,17 +68,19 @@ const addLanguageToPathname = (url: URL, language: LanguageType) =>
   return removeTrailingSlash(modifiedUrl);
 };
 
-const validateDefaultLanguage = (url: URL) =>
-{
+const validateDefaultLanguage = (url: URL) => {
   const urlLang = url.pathname.split('/')[1];
 
   return urlLang === defaultLanguage;
 };
 
-const getI18n = <T>(language: LanguageType, languageModule: Record<string, T>): T => languageModule[language] as T;
+const getI18n = <T>(language: LanguageType, languageModule: Record<string, T>): T => {
+  return languageModule[language] as T;
+};
 
 export {
   getI18n,
+  validateLanguage,
   getStaticLanguage,
   getLanguageFromUrl,
   getLanguagePathname,
